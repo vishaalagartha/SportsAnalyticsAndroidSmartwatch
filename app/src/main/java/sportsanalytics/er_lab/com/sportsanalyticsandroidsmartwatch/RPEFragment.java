d0ee973afe9cb18c30a485de543c20eebeac343e
vishaalagartha <vishaalagartha@gmail.com>
2018-02-20 17:02:26 -0800

    Fixes constraints/layouts for round screen

--- app/src/main/java/sportsanalytics/er_lab/com/sportsanalyticsandroidsmartwatch/RPEFragment.java
+++ app/src/main/java/sportsanalytics/er_lab/com/sportsanalyticsandroidsmartwatch/RPEFragment.java
@@ -1,12 +1,22 @@
 package sportsanalytics.er_lab.com.sportsanalyticsandroidsmartwatch;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.android.volley.Request;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
 
 
 /**
@@ -17,7 +27,7 @@ import android.view.ViewGroup;
  * Use the {@link RPEFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RPEFragment extends Fragment {
+public class RPEFragment extends Fragment implements View.OnClickListener {
     private static final String ARG_FIRST_NAME = "firstname";
     private static final String ARG_LAST_NAME = "lastname";
     private static final String ARG_TEAM = "teamname";
@@ -26,6 +36,7 @@ public class RPEFragment extends Fragment {
     private String mFirstName;
     private String mLastName;
     private Team mTeam;
+    private Button submitButton;
 
     private OnFragmentInteractionListener mListener;
 
@@ -57,13 +68,18 @@ public class RPEFragment extends Fragment {
             mLastName = getArguments().getString(ARG_LAST_NAME);
             mTeam = (Team) getArguments().getSerializable(ARG_TEAM);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rpe, container, false);
+        View view =  inflater.inflate(R.layout.fragment_rpe, container, false);
+        submitButton = (Button) view.findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -102,5 +118,41 @@ public class RPEFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onClick(View v) {
+        //do what you want to do when button is clicked
+        Log.d("TAG", "submit");
+/*
+        new NetworkManager().request(new RequestInterface() {
+            @Override
+            public void onRequestSuccess(String response) {
+                Log.d("TAG", "response: " + response);
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
+                    ArrayList<String> athletesArray = new ArrayList<String>();
+                    if (jsonArray != null) {
+                        for (int i=0;i<jsonArray.length();i++){
+                            athletesArray.add(jsonArray.getString(i));
+                        }
+                    }
+                    Intent athletesIntent = new Intent(getActivity().getApplicationContext(), AthletesActivity.class);
+                    athletesIntent.putExtra("team", team);
+                    athletesIntent.putExtra("athletes", athletesArray);
+                    startActivity(athletesIntent);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onRequestFailure(String error) {
+                Log.e("ERROR: ", error);
+            }
+        }, new URLs().getAthletesUrl() + "?team=" + team.mName, Request.Method.POST, params, headers, getActivity().getApplicationContext());
+
+    }*/
     }
 }
